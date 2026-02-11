4cfdc85759eed69464d919c8f22410dbea737c7a
Sujoy Dam <sujooy01@gmail.com>
2026-02-11 12:20:27 +0600

    create db table

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,19 @@ const initDB = async () => {
     updated_at TIMESTAMP DEFAULT NOW()
     )
     `)
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS todos(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    complete BOOLEAN DEFAULT false,
+    due_dat DATE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `)
 }
 initDB()
 
